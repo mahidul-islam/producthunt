67ad1d24dd34eebb1e6787291f222e5313fe6740
mahidul-islam <mizihan84@gmail.com>
2018-06-09 11:29:58 +0600

    Added auth stuff product and account app and urls base.html added database postgresql

---
+++ account/views.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.shortcuts import render, redirect
+
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'account/signup.html', {'error':'Username has already been taken'})
+            except:
+                user = User.objects.create_user(request.POST['username'], request.POST['password2'])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            return render(request, 'account/signup.html', {'error':'Password must match'})
+
+    else:
+        return render(request, 'account/signup.html')
+
+def login(request):
+    if request.method == 'POST':
+        user = auth.authenticate(username = request.POST['username'], password = request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'account/login.html', {'error': 'Username or Password is Incorrect.'} )
+    else:
+        return render(request, 'account/login.html')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
